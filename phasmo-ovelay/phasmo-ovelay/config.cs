36417932518a27b5b0e6ea42da541620d8ffb94d
Iván Menacho Domínguez <92464787+MenachoRBB@users.noreply.github.com>
2024-10-02 22:55:42 +0200

    default keys config

--- phasmo-ovelay/phasmo-ovelay/config.cs
+++ phasmo-ovelay/phasmo-ovelay/config.cs
@@ -5,9 +5,9 @@ namespace phasmo_ovelay
 {
     public class Config
     {
-        public string StartStopKey { get; set; }
-        public string ResetKey { get; set; }
-        public string ToggleVisibilityKey { get; set; }
+        public string StartStopKey { get; set; } = "D1"; // Valor por defecto
+        public string ResetKey { get; set; } = "D2"; // Valor por defecto
+        public string ToggleVisibilityKey { get; set; } = "D3"; // Valor por defecto
 
         // Cargar la configuración desde el archivo JSON
         public static Config LoadFromFile(string filePath)
@@ -15,9 +15,19 @@ namespace phasmo_ovelay
             if (File.Exists(filePath))
             {
                 string json = File.ReadAllText(filePath);
-                return JsonConvert.DeserializeObject<Config>(json);
+                var config = JsonConvert.DeserializeObject<Config>(json);
+
+                // Comprobar si las claves son válidas
+                if (string.IsNullOrEmpty(config.StartStopKey) ||
+                    string.IsNullOrEmpty(config.ResetKey) ||
+                    string.IsNullOrEmpty(config.ToggleVisibilityKey))
+                {
+                    return new Config(); // Devuelve la configuración por defecto
+                }
+
+                return config;
             }
-            return new Config(); // Devuelve una nueva instancia si el archivo no existe
+            return new Config(); // Devuelve una nueva instancia con valores por defecto si el archivo no existe
         }
     }
 }
--- phasmo-ovelay/phasmo-ovelay/obj/Debug/net8.0-windows/phasmo-ovelay.AssemblyInfo.cs
+++ phasmo-ovelay/phasmo-ovelay/obj/Debug/net8.0-windows/phasmo-ovelay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("phasmo-ovelay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1daca29434c8ca1cf4d033f0afe554ad2bdfe920")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf7d3144fcd2653d6346f7614b39ac463796628")]
 [assembly: System.Reflection.AssemblyProductAttribute("phasmo-ovelay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("phasmo-ovelay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
